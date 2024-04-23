5c6aac8e506bf08ba328bc5f3a0b56e06ac91ea3
SimoneBedendi <167210016+SimoneBedendi@users.noreply.github.com>
2024-04-23 15:03:55 +0200

    Fixi first FSA
    Fixed first FSA:first line production works!

--- src/my/company/PL1State.java
+++ src/my/company/PL1State.java
@@ -18,107 +18,113 @@ import com.ttsnetwork.modulespack.conveyors.SensorCatch;
  * @author Simone
  */
 public class PL1State extends StateMachine {
-    
-  double VROB = 3000;
-  
+
+    double VROB = 6000;
+
     IRobotCommands rob;
-  
+
     IConveyorCommands Cb;
     IConveyorCommands Cd;
-    
+
     ISensorProvider CbSen1;
     ISensorProvider CdSen1;
-    
+
     SimpleStateVar partB = new SimpleStateVar();
     SimpleStateVar partD = new SimpleStateVar();
     SimpleStateVar finished = new SimpleStateVar();
-    
+
     ConveyorBox workingB;
     ConveyorBox workingD;
-    
-    
+
     @Override
     public void onInit() {
-       rob = useSkill(IRobotCommands.class, "R1");
+        rob = useSkill(IRobotCommands.class, "R1");
 
-       Cb = useSkill(IConveyorCommands.class, "CB");
-       Cd = useSkill(IConveyorCommands.class, "CD2");
-       
-       CbSen1 = useSkill(ISensorProvider.class, "CB");
-       CdSen1 = useSkill(ISensorProvider.class, "CD2");
+        Cb = useSkill(IConveyorCommands.class, "CB");
+        Cd = useSkill(IConveyorCommands.class, "CD2");
 
-       CbSen1.registerOnSensors(this::b_sensor1, "B_sen1");
-       CdSen1.registerOnSensors(this::d_sensor1, "D2_sen1");
-       
+        CbSen1 = useSkill(ISensorProvider.class, "CB");
+        CdSen1 = useSkill(ISensorProvider.class, "CD2");
+
+        CbSen1.registerOnSensors(this::b_sensor1, "B_sen1");
+        CdSen1.registerOnSensors(this::d_sensor1, "D2_sen1");
+
     }
+
     @Override
     public void onStart() {
         switchState(100);
     }
-    public void b_sensor1(SensorCatch sc){
-    schedule.startSerial();
-    Cb.lock(sc.box);
-        setVar(partB, sc.box);
-    schedule.end();
-    }
-    public void d_sensor1(SensorCatch sc){
+
+    public void b_sensor1(SensorCatch sc) {
         schedule.startSerial();
-        Cd.lock(sc.box);
-            setVar(partD, sc.box);
+        Cb.lock(sc.box);
+        setVar(partB, sc.box);
         schedule.end();
     }
-    public void state_100(){
-        if(partB != null){
+
+    public void d_sensor1(SensorCatch sc) {
+        schedule.startSerial();
+        Cd.lock(sc.box);
+        setVar(partD, sc.box);
+        schedule.end();
+    }
+
+    public void state_100() {
+        if (partB.read() != null) {
             workingB = partB.readAndForget();
             switchState(200);
         }
     }
-    
-    public void state_200(){
-        if(partD != null){
+
+    public void state_200() {
+        if (partD.read() != null) {
             workingD = partD.readAndForget();
             switchState(300);
         }
-    
+
     }
-        
-    public void AssmBD(){
-    schedule.startSerial();
+
+    public void AssmBD() {
+        schedule.startSerial();
         rob.moveLinear(driver.getFrameTransform("CD2.midPos"), VROB);
         rob.moveLinear(BoxUtils.targetTop(workingD), VROB);
         rob.pick(workingD.entity);
         Cd.remove(workingD);
-        rob.moveLinear(driver.getFrameTransform("B.midPos"), VROB);
-        rob.moveLinear(BoxUtils.targetOffset(workingB, 0, 0, 400, 0, 0, 0), VROB);
-        rob.move(BoxUtils.targetTop(workingB), workingB.cF, 1000L);
+        rob.moveLinear(driver.getFrameTransform("CD2.midPos"), VROB);
+        rob.moveLinear(driver.getFrameTransform("CB.midPos"), VROB);
+     //   rob.moveLinear(BoxUtils.targetTop(workingB), VROB);
+        rob.move(BoxUtils.targetTop(workingB), workingD.cF, VROB);
         rob.release();
         rob.home();
-        schedule.attach(workingB.entity, workingB.entity);
+        schedule.attach(workingD.entity, workingB.entity);
         setVar(finished, true);
-    schedule.end();
+        schedule.end();
     }
-    
 
-    public void state_300(){
+    public void state_300() {
         AssmBD();
         switchState(400);
     }
-    public void state_400(){
-        if(finished.readBoolean()){
+
+    public void state_400() {
+        if (finished.readBoolean()) {
             finished.write(false);
-            switchState(500); 
+            switchState(500);
         }
     }
-    public void state_500(){
+
+    public void state_500() {
         releasebox();
         switchState(100);
     }
-    public void releasebox(){
-        
+
+    public void releasebox() {
+
         schedule.startSerial();
-            Cb.release(workingB);
+        Cb.release(workingB);
         schedule.end();
-        
+
     }
-    
+
 }
